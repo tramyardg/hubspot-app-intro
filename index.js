953c95a64bc3fabe4468b2021ea4e1a60b6cc082
raydguzman <rguzman@parkour3.com>
2021-08-01 21:41:46 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express")
 const axios = require("axios")
 
+require('dotenv').config()
+
 const app = express()
 
+const API_KEY_TEST_PORTAL = process.env.API_KEY_TEST_PORTAL;
+
+app.get('/contact', async (req, res) => {
+    const contacts = `https://api.hubapi.com/contacts/v1/lists/all/contacts/recent?hapikey=${API_KEY_TEST_PORTAL}`
+    try {
+        const resp = await axios.get(contacts);
+        const data = resp.data;
+        res.json(data)
+    } catch (error) {
+        console.error(error)
+    }
+})
 
 app.listen(3000, () => console.log(`Listening on http://localhost:3000`))
